f8a11f19d13bba5271137399ab97042f8c2de35e
interads@DESKTOP-VKGQQDG <interads.dekstop-vkgqqdg@interads.com>
2022-08-09 15:48:42 +0700

    add seeder script

---
+++ api/apps/utils/seed.ts
@@ -0,0 +1,72 @@
+import { PrismaClient } from '@prisma/client'
+import * as bcryptjs from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.$connect()
+
+  await prisma.user.deleteMany({})
+  await prisma.question.deleteMany({})
+  await prisma.answer.deleteMany({})
+  await prisma.like.deleteMany({})
+  await prisma.advice.deleteMany({})
+
+  await prisma.user.create({
+    data: {
+      username: 'admin',
+      password: bcryptjs.hashSync('HuceOdw128379%^92198hhe12189@89', 12),
+    },
+  })
+
+  console.log('success create user')
+
+  await prisma.question.create({
+    data: {
+      question: 'siapa nama bapak dari jokowi'.toLowerCase(),
+      answer: {
+        create: {
+          answer: 'Siapa aja boleh'.toLowerCase(),
+        },
+      },
+    },
+  })
+
+  const createdAdvice = await prisma.advice.create({
+    data: {
+      advice: 'Test advice 1',
+    },
+  })
+
+  await prisma.like.createMany({
+    data: [
+      {
+        adviceId: createdAdvice.id,
+      },
+      {
+        adviceId: createdAdvice.id,
+      },
+      {
+        adviceId: createdAdvice.id,
+      },
+    ],
+  })
+
+  const user = await prisma.user.findFirst({
+    where: {
+      username: 'admin',
+    },
+  })
+
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
